06d4c494acbb54a2facdb420347fd403b75ac378
analgotrader <oneangrytrader@gmail.com>
2022-05-13 16:24:25 +0200

    .

--- IB.Api.Client/Helper/DateHelper.cs
+++ IB.Api.Client/Helper/DateHelper.cs
@@ -8,12 +8,15 @@ namespace IB.Api.Client.Helper
         {
             return dateTime.ToString("yyyyMMdd HH:mm:ss");
         }
-
         public static DateTime UnixTimeStampToDateTime(double time)
         {
             var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             dateTime = dateTime.AddSeconds(time).ToLocalTime();
             return dateTime;
+        }
+        public static long DateToEpoch(DateTime date, long multiplier = 1)
+        {
+            return Convert.ToInt64((date - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds * multiplier);
         }
     }
 }

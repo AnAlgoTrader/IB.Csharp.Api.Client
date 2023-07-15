92939214b09849243813a99f8c64805a7a6e8c5a
AnAlgoTrader <an.algo.trader@gmail.com>
2023-07-15 14:53:15 +0200

    .

--- IB.Api.Client/Client/Model/OptionParameterDefinition.cs
+++ IB.Api.Client/Client/Model/OptionParameterDefinition.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Text;
 
 namespace IB.Api.Client.Model
 {
@@ -10,5 +11,22 @@ namespace IB.Api.Client.Model
         public string Multiplier { get; internal set; }
         public HashSet<string> Expirations { get; internal set; }
         public HashSet<double> Strikes { get; internal set; }
+
+        public override string ToString()
+        {
+            var output = new StringBuilder();
+            output.AppendLine($"Exchange:{Exchange} UnderlyingConId:{UnderlyingConId} TradingClass:{TradingClass} Multiplier:{Multiplier}");
+            output.AppendLine("Expirations:");
+            foreach (var expiration in Expirations)
+            {
+                output.AppendLine($"\t{expiration}");
+            }
+            output.AppendLine("Strikes:");
+            foreach (var strike in Strikes)
+            {
+                output.AppendLine($"\t{strike}");
+            }
+            return output.ToString();
+        }
     }
 }
